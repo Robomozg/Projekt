8d3e129cc508ce80e85c61964aa11e358575b2be
Damian <77362018+xN3ONx@users.noreply.github.com>
2021-01-13 17:52:08 +0100

    Update Protagonista.cs

--- Protagonista.cs
+++ Protagonista.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,5 +30,27 @@ namespace Projekt_Alfa
             this.wartosc = wartosc;
             this.opis = opis;
         }
+    }
+    public class Wiedza
+    {
+        string nazwa;
+        int poziom;
+        string opis;
+        public Wiedza(string nazwa, int poziom, string opis)
+        {
+            this.nazwa = nazwa;
+            this.poziom = poziom;
+            this.opis = opis;
+        }
+        public string Nazwa { get => nazwa; protected set => nazwa = value; }
+
+        public int Poziom { get => poziom; 
+       
+            protected set 
+            {
+                if (value <= 100) poziom = value;
+            }
+        }
+        public string Opis { get => opis; protected set => opis = value; }
     }
 }
